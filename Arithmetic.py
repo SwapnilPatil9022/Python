eb2cd0e66d2326024827dcd75e9cc0f9c08a7bf6
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-01-15 14:20:31 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,15 @@
+def Add(No1, No2):
+    Ans = No1 + No2
+    return Ans
+
+def Sub(No1, No2):
+    Ans = No1 - No2
+    return Ans
+    
+def Mult(No1, No2):
+    Ans = No1 * No2
+    return Ans
+    
+def Div(No1, No2):
+    Ans = No1 / No2
+    return Ans
