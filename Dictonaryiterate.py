e98232e650adbdbafa8c40ee6f5d4f2a2620141e
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-02-14 11:33:50 +0530

    Add files via upload

---
+++ Dictonaryiterate.py
@@ -0,0 +1,23 @@
+Batches = {"PPA" : 18000, "LB" : 16700, "Python" : 16500, "Angular" : 15000}
+
+print("Data of Dictonary : ",Batches)
+
+print("Data traversal using for loop")
+for x in Batches:
+    print(x)
+                                            # OR
+print("Data traversal using for loop")
+for x in Batches:
+    print(x,Batches[x])
+                                            # OR
+print("Data traversal using for loop")
+for x in Batches:
+    print(x,Batches.get(x))
+                                                
+keyBatches = Batches.keys()
+print(type(keyBatches))
+print(keyBatches)
+
+valueBatches = Batches.values()
+print(type(valueBatches))
+print(valueBatches)
