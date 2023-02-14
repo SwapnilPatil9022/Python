30e47f8df9905df0e22c713c836ae946e29962dd
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-02-14 11:31:14 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,20 @@
+print("Demonstrat of built in data types")
+
+# int iobj = 11;                        # Static type
+iobj = 11                               # Dynamic type
+
+fobj = 3.14
+
+sobj = "Hello"
+
+bobj = True
+
+print(iobj)
+print(fobj)
+print(sobj)
+print(bobj)
+
+print(type(iobj))
+print(type(fobj))
+print(type(sobj))
+print(type(bobj))
