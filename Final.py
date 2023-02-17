133b167f149e01b60ed778e85a70c0d34bd0f418
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-02-17 21:54:39 +0530

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,12 @@
+print("Marvellous Infosystem")
+
+def main():
+	print("Jay Ganesh...")
+	print("Inside main")
+	
+if __name__ == "__main__":
+    print("Inside starter")
+    
+main()          #Function call
+    
+print("End of program")
---
+++ First (2).py
@@ -0,0 +1,1 @@
+printf("Jay Ganesh..")
