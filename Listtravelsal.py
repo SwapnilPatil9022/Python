97f368cd413f961471d1c9d442d006871c07d740
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-02-22 10:32:02 +0530

    Add files via upload

---
+++ Listtravelsal.py
@@ -0,0 +1,20 @@
+
+value = [10,20,30,40,50]
+
+print(value[0])
+print(value[1])
+print(value[2])
+print(value[3])
+print(value[4])
+
+print("-----------------------------------------")
+
+for i in range(0,len(value)):
+    print(value[i])
+    
+print("------------------------------------------")
+                           # OR
+for no in value:
+    print(no)
+    
+print("-------------------------------------------")
