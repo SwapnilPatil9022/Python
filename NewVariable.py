0b32bc528d84ee6d0174efeab660185b5aa015ea
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-02-24 10:53:25 +0530

    Add files via upload

---
+++ NewVariable.py
@@ -0,0 +1,11 @@
+no = 11
+
+print("Value of no : ",no)
+print("Data type of no is : ",type(no))
+print("ID of no is : ",id(no))
+
+i = 11
+
+print("Value of no : ",i)
+print("Data type of no is : ",type(i))
+print("ID of no is : ",id(i))
