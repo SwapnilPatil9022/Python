73f1880ec73732799330e2cb662908d51a8e3847
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-03-05 20:06:08 +0530

    Add files via upload

---
+++ program1.1.py
@@ -0,0 +1,10 @@
+# 1. Write a program which containcs one function named as fun(). The function should display "Hello from Fun" on console.
+
+def Display():
+    print("Hello from Fun...")
+    
+def main():
+    Display()
+    
+if __name__ == "__main__":
+    main()
