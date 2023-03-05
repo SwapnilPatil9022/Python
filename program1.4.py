4454d181b32fc1ab6b192f5ff3315965b6676fb2
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-03-05 20:07:53 +0530

    Add files via upload

---
+++ program1.4.py
@@ -0,0 +1,25 @@
+# Write a program which display 5 times Marvellous on screen .
+
+def main():
+    value = "Marvellous..."
+    
+    for i in range(0,5):       #using for loop.         
+        print(value)
+    
+if __name__ == "__main__":
+    main()
+
+
+
+"""
+def main():
+    i = 0
+    
+    while i < 5:                            # using while loop.
+        print("Marvellous...")
+        i = i + 1
+        
+if __name__ == "__main__":
+    main()
+"""
+
