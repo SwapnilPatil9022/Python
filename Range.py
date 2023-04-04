4ad7653bdf21f4407f87f745984d799a49dfd3c8
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-04-04 21:37:39 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,9 @@
+
+# range(start, end, displacement)       #desplacement mhanje kiti ne pudhe jaych
+# start is by default ____
+# end : there is no default value
+# desplacement : is by default 1
+
+range(1,5)      # 1 2   3   4
+range(1,5,1)    # 1 2   3   4   
+range(1,10,2)   # 1 3   5   7   9   
