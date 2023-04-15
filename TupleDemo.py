d640e5c15a25ee2f79edee5cd67e3a7e55a1d279
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-04-15 22:00:29 +0530

    Add files via upload

---
+++ TupleDemo.py
@@ -0,0 +1,16 @@
+print("Demonstration of Tuple")
+
+# Data : Hetrogeneous
+# Orderd : Yes
+# Indexed : Yes
+# Mutable : No          # InMutable : Yes
+# Duplicates : Yes
+
+data = (11,21,51,101,21,11)                       # Duplicates
+data1 = (11, 90.80, True, "Hello")          #Hetrogeneous
+
+print("Data is Hetrogeneous : ",data1)
+print("Data is orderd : ",data1)
+print("Data is index 2 : ",data[2])
+print("Data with duplicate elements : ",data)
+
